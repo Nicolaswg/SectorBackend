2481879f41ff0370f19fc04339e5326c8cf299fb
Nicolaswg <nigonbol123@gmail.com>
2023-11-25 13:34:08 -0400

    Add controller for getting sectors for database && post a new user with a sector

---
+++ src/controllers/sector.controller.js
@@ -0,0 +1,31 @@
+import { connectDB, disconnectDB } from '../db.js'
+import Sector from '../models/sector.model.js'
+import User from '../models/user.model.js'
+
+export const getSector = async (req, res) => {
+  try {
+    connectDB()
+    const sectors = await Sector.find().sort({ name: 1 })
+    return res.status(200).json(sectors)
+  } catch (err) {
+    res.status(404).json({ message: err.message })
+  } finally {
+    await disconnectDB()
+  }
+}
+
+export const postUserSector = async (req, res) => {
+  const { name, sector, terms } = req.body
+  if (!name || !sector || !terms) return res.status(400).json({ message: 'All fields are required' })
+
+  try {
+    connectDB()
+    const user = await User.findOne({ name })
+    if (user) return res.status(409).json({ error: 'User already exists' })
+    const newUser = new User({ name, sector, terms })
+    await newUser.save()
+    return res.status(201).json({ message: 'User created successfully' })
+  } catch (err) {
+    return res.status(500).json({ error: err })
+  }
+}
