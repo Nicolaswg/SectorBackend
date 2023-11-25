bea41910d62e2fae18b6f199a19e25ab0846fd15
Nicolaswg <nigonbol123@gmail.com>
2023-11-25 15:04:10 -0400

    Add home route  message || implement validation middleware for sector controller

--- src/routes/sector.routes.js
+++ src/routes/sector.routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
 import { getSector, postUserSector } from '../controllers/sector.controller.js'
-
+import validator from '../validators/sectors.validators.js'
 const sectorRouter = Router()
 
 sectorRouter.get('/sectors', getSector)
 
-sectorRouter.post('/sectors/user', postUserSector)
+sectorRouter.post('/sectors/user', validator, postUserSector)
 
 export default sectorRouter
